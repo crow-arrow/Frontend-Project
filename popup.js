a2e687d6cfe190f5bf9d4d14fd8c08d8ca5bbb25
amal_yuldashev <amalyuldashev@gmail.com>
2025-01-09 21:55:43 +0100

    work on input style

--- popup.js
+++ popup.js
@@ -14,15 +14,33 @@ closeButton.addEventListener('click', () => {
     popup.classList.add('hiden');
 });
 
-popup.addEventListener('click', (event) => {
-    if (event.target === popupBody) {
-        popup.classList.add('hiden');
-    }
-});
+// popup.addEventListener('click', (event) => {
+//     if (event.target === popupBody) {
+//         popup.classList.add('hiden');
+//     }
+// });
 
 downloadButton.addEventListener('click', () => {
     const link = document.createElement('a');
     link.href = './images/pdf/Lebenslauf.pdf';
     link.download = 'Lebenslauf.pdf';
     link.click();
+});
+
+document.getElementById('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    this.classList.add('form-submitted');
+});
+
+// Дополнительно для обработки потери фокуса
+document.querySelectorAll('.textbox input').forEach(input => {
+    input.addEventListener('blur', function() {
+        if (this.value === '') {
+        this.form.classList.add('form-submitted');
+        }
+    });
+});
+
+document.querySelector('.pop_up_close').addEventListener('click', function() {
+    document.getElementById('form').classList.remove('form-submitted');
 });
